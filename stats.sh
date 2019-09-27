9adce51ec7cc0210b88d80bc0aa7624154b1a435
rh4p2404 <46589877+rh4p2404@users.noreply.github.com>
2019-09-27 16:04:46 +0300

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -7,7 +7,7 @@ for year in ${years[*]}; do
 	-e 'select day, count(*) from <tbl_name> where day >=${year}0101 and day <=${year}0630 group by day order by day desc;' -hivevar year=${year} > ~/wsrm_stats_${year}_1.txt
 	beeline -u "jdbc:hive2://bda41node04:10000/;principal=hive/bda41node04.moscow.alfaintra.net@moscow.alfaintra.net" --outputformat=tsv2 \
 	-e 'select day, count(*) from <tbl_name> where day >=${year}0701 and day <=${year}1231 group by day order by day desc;' -hivevar year=${year} > ~/wsrm_stats_${year}_2.txt
-	python /home/l_hdb/wsrm_stats_create.py ${year} 
+	python ~/compute_stats_create.py ${year} 
 	impala-shell -i bda41node03  -f /home/l_hdb/<tbl_name>_stats_${year}_1.sh
 	impala-shell -i bda41node03  -f /home/l_hdb/<tbl_name>_stats_${year}_2.sh
 done
